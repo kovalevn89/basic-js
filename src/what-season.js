b961773607bb974ec4bdc867584a06c6454bc9a1
Niklay Kovalev <kovalev.nikolay.89@gmail.com>
2022-10-13 23:54:47 +0300

    feat: add Dream team and What season?

--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  try {
+    date.getTime()
+  } catch {
+    throw new NotImplementedError('Invalid date!');
+  }
+
+  if (!date)
+    return 'Unable to determine the time of year!';
+
+
+  if (date.getMonth() <= 1 || date.getMonth() === 11)
+    return 'winter';
+
+  if (date.getMonth() <= 4)
+    return 'spring';
+
+  if (date.getMonth() <= 7)
+    return 'summer';
+
+  if (date.getMonth() <= 10)
+    return 'autumn';
 }
 
 module.exports = {
