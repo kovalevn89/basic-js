4a0794a391e65fee489af0429b7b652b45d29ace
Kovalev Nikolay <kovalev.nikolay.89@gmail.com>
2022-10-14 15:55:39 +0300

    feat: add hanoi tower and transform array

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -4,26 +4,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Implement chainMaker object according to task description
  * 
  */
+
+var chain =[];
 const chainMaker = {
+  
   getLength() {
-    throw new NotImplementedError('Not implemented');
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    return chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  addLink( value ) {
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    chain.push(value);
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
+  removeLink( position ) {
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    chain.splice(position, 1);
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    chain.reverse();
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    return '( ' + chain.join(' )~~( ') + ' )';
   }
 };
 
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform( arr ) {
+
+  try{
+      let _arr = arr.slice(0);
+      
+      let result = [];
+      for(let i = 0; i< _arr.length; i++){
+        switch(_arr[i]){
+          case '--discard-next': {i=i + 1; if(i!=_arr.length-1) _arr[i + 1] = 'dis'; } break;
+          case '--discard-prev': {result.pop();} break;
+          case '--double-next': {if(i!=_arr.length-1) result.push(_arr[i+1]);} break;
+          case '--double-prev': {if(i!=0) result.push(_arr[i-1]);} break;
+          default: {if(_arr[i]!='dis') result.push(_arr[i]);} break;
+        }
+      }
+
+      return result;
+  }catch{
+    throw new NotImplementedError("'arr' parameter must be an instance of the Array!");
+  }
 }
 
 module.exports = {
